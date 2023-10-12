6f2b97ca4630323ad1855c3b033b99cc6df456d8
Anthony Baran <abaran7@yahoo.com>
2023-10-12 23:43:32 +0000

    Added the pheonix libraries and header files. Implemented motor functions in manual control node.

--- src/manual_control_pkg/src/manual_control.cpp
+++ src/manual_control_pkg/src/manual_control.cpp
@@ -2,6 +2,12 @@
 #include "sensor_msgs/msg/joy.hpp"
 #include "manual_control_cpp_pkg/DifferentialDrive.hpp"
 
+#define Phoenix_No_WPI
+#include "ctre/Phoenix.h"
+#include "ctre/phoenix/platform/Platform.hpp"
+#include "ctre/phoenix/unmanaged/Unmanaged.h"
+#include "ctre/phoenix/cci/Unmanaged_CCI.h"
+
 /*
 Author: Anthony Baran
 
@@ -9,6 +15,10 @@ Node that subscribes to the joy topic and uses joystick input to move robot.
 Motor functions still need to be implemented. Right now left and right wheel percent output
 are printed to the terminal. 
 */
+
+std::string interface = "can0";
+TalonFX left_wheel_motor(1, interface);
+TalonFX right_wheel_motor(0, interface);
 
 class ManualControl : public rclcpp::Node
 {
@@ -30,7 +40,15 @@ private:
 
         DifferentialDrive d(x, y);
 
-        RCLCPP_INFO(this->get_logger(), " theta = %0.4f right_wheel = %0.4f left_wheel = %0.4f", d.gettheta(), d.calculate_wheel_percentOutput()[0], d.calculate_wheel_percentOutput()[1]);
+        left_wheel_motor.Set(ControlMode::PercentOutput, d.calculate_wheel_percentOutput()[1]);
+        right_wheel_motor.Set(ControlMode::PercentOutput, d.calculate_wheel_percentOutput()[0]);
+
+        RCLCPP_INFO(this->get_logger(), "right_wheel = %0.4f left_wheel = %0.4f", d.calculate_wheel_percentOutput()[0], d.calculate_wheel_percentOutput()[1]);
+    }
+
+    void callbackMotorPublisher()
+    {
+
     }
 
     rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr controller_subscriber;
