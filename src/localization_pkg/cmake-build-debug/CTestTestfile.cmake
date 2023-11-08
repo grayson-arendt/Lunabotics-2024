37d250f0f1588a1eac438446a115fd4bf5e47a1f
grayson-arendt <grayson.n.arendt5@gmail.com>
2023-11-08 13:18:21 -0600

    Removed cmake-build-debug

--- src/localization_pkg/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/grayson/ros2_ws/src/src/localization_pkg
-# Build directory: /home/grayson/ros2_ws/src/src/localization_pkg/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
