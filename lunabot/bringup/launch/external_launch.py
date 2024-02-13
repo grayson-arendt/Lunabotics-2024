2131042cad22ee1e5b01f1914936ce9dafcfcfc3
Grayson Arendt <grayson.n.arendt5@gmail.com>
2024-02-13 00:23:48 -0500

    Extra document formatting.

--- lunabot/bringup/launch/external_launch.py
+++ lunabot/bringup/launch/external_launch.py
@@ -1,23 +1,32 @@
 from launch.substitutions import LaunchConfiguration
 
+
 def generate_launch_description():
 
     DeclareLaunchArgument(
-            'namespace', default_value='',
-            description='Top-level namespace'),
+        "namespace", default_value="", description="Top-level namespace"
+    ),
 
     DeclareLaunchArgument(
-            'use_sim_time', default_value='false',
-            description='Use simulation (Gazebo) clock if true'),
+        "use_sim_time",
+        default_value="false",
+        description="Use simulation (Gazebo) clock if true",
+    ),
 
     DeclareLaunchArgument(
-            'autostart', default_value='true',
-            description='Automatically startup the nav2 stack'),
-    
+        "autostart",
+        default_value="true",
+        description="Automatically startup the nav2 stack",
+    ),
 
-    rviz_nav2_plugins_launch_file_path = os.path.join(get_package_share_path('nav2_bringup'),'launch','rviz_launch.py')
+    rviz_nav2_plugins_launch_file_path = os.path.join(
+        get_package_share_path("nav2_bringup"), "launch", "rviz_launch.py"
+    )
 
-    return LaunchDescription([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(rviz_nav2_plugins_launch_file_path))
-    ])
+    return LaunchDescription(
+        [
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(rviz_nav2_plugins_launch_file_path)
+            )
+        ]
+    )
